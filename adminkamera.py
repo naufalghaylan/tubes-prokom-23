77b54728408c20d30181a7ec5674f9eb3ade7f33
Ghaylan Arput <naufalghaylan@gmail.com>
2022-05-25 18:20:54 +0700

    Tambah CSV

--- adminkamera.py
+++ adminkamera.py
@@ -1,3 +1,18 @@
+from csv import DictWriter
+
+
 print("Selamat Datang di program admin kamera")
-print("Tes")
-print("Coba")
+nama = input("Masukkan nama penyewa= ")
+usia = int(input("Masukkan usia penyewa= "))
+no_id = int(input("Masukkan nomer identidas penyewa= "))
+alamat = input("Masukkan alamat penyewa= ")
+telefon = int(input("masukkan nomor telefon penyewa= "))
+
+penyewa_header = ['Nama', 'Usia', 'No_Identitas', 'Alamat', 'Telefon']
+penyewa_data = [{'Nama' : nama, 'Usia' : usia, 'No_Identitas' : no_id, 'Alamat' : alamat, 'Telefon' : telefon}]
+
+with open('peminjam.csv', 'a', newline='') as csv_peminjam:
+    peminjam_writer = DictWriter(csv_peminjam, fieldnames=penyewa_header)
+    peminjam_writer.writerows(penyewa_data)
+
+    
